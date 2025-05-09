23aa9557cb2f002d35da4f32cab7fb383b5a8385
rem-aster <petr.omlet@gmail.com>
2025-05-09 21:30:35 +0300

    a

---
+++ webapp/frontend/src/components/ui/Activity.vue
@@ -0,0 +1,18 @@
+<template>
+  <li class="list-row">
+    <div class="font-semibold">
+      {{ activityType }}
+    </div>
+    <div>
+      {{ props.content }}
+    </div>
+  </li>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  content?: string
+  activityType?: string
+  activityTime?: string
+}>()
+</script>
